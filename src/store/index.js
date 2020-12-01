c99f4574a6b614b8305110f05b7f2550b448866e
r0bban <robert.p.svensson@gmail.com>
2020-12-01 01:39:26 +0100

    :art: added auth api

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import authModule from "@/store/modules/auth"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    authModule
+  },
   state: {
+    error: ""
   },
   mutations: {
+    setError(state, error) {
+      state.error = error
+    }
   },
   actions: {
+    setNewError({commit}, error){
+      commit('setError', error)
+    }
   },
-  modules: {
+  getters: {
+    // getUser(state){
+    //   return state.auth.currentUser
+    // }
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <button @click="login">Logga in</button>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                inloggningsuppg: {
+                    username: "superadmin",
+                    password: "BOBbo9tti!"
+                }
+            }
+
+        },
+        methods: {
+            login() {
+                this.$store.dispatch("authUser", this.inloggningsuppg)
+            }
+        }
+    }
+
+</script>
